1e6739e262a53b15ff637385dee1622100dbf9c2
Arvind-4 <lordarvindthegamer@gmail.com>
2022-04-24 13:52:43 +0530

    Updated: formater for python

--- commands/format.sh
+++ commands/format.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-black web --exclude '__pycache__' --exclude 'migrations' --check --verbose
+echo "Checking files to format..."
+black web --exclude '__pycache__' --exclude 'migrations' --ckeck --verbose
+
+echo "Formatting ..."
+black web --exclude '__pycache__' --exclude 'migrations'
+echo "Done :)"
